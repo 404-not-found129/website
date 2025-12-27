c20db4a67b0d0c5cfb69e28b7f5c45d3844d0bf8
jlcgaming2231 <jackson.leo.clement@gmail.com>
2025-12-27 12:53:24 -0500

    working calculator page

--- public/script.js
+++ public/script.js
@@ -1,29 +1,66 @@
-const input=document.getElementById('num-input');
-const addButton=document.getElementById('add-button');
-const subtractButton=document.getElementById('subtract-button');
+const languageSelect=document.getElementById('language-select');
+const input = document.getElementById('num-input');
+const addButton = document.getElementById('add-button');
+const subtractButton = document.getElementById('subtract-button');
+const operations = document.getElementById('operations');
+const clearButton = document.getElementById('clear-button');
+const calculateButton = document.getElementById('calculate-button');
+const resultText = document.getElementById('result');
 
+let numberList = [];
 
-console.dir({input});
+console.dir({input, addButton, subtractButton, operations});
 
-input.addEventListener('input', function() {
+input.addEventListener('input', function () {
   console.log('Input value changed:', input.value);
-  if (input.value <1 ) {
-    input.value=1;
-  }else if (input.value > 67) {
-    input.value=67;
+  if (input.value < 1) {
+    input.value = 1;
+  } else if (input.value > 67) {
+    input.value = 67;
   }
 });
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function () {
   console.log('Add button clicked');
-  if (input.value < 67 ) {
-    input.value++;
-  }
+  appendOperation(+input.value);
 });
 
-subtractButton.addEventListener('click', function() {
+subtractButton.addEventListener('click', function () {
   console.log('Subtract button clicked');
-  if (input.value > 1 ) {
-    input.value--;
-  }
+  appendOperation(-input.value);
 });
+
+calculateButton.addEventListener('click', function () {
+  console.log('Calculate button clicked');
+  const result = numberList.reduce((acc, curr) => acc + curr);
+  resultText.innerText = result;
+  const language = languageSelect.value;
+  console.log({
+    language,
+    result,
+    numberList
+  });
+});
+
+clearButton.addEventListener('click', function () {
+  console.log('Clear button clicked');
+  operations.innerHTML = '';
+  numberList = [];
+  resultText.innerText = '';
+  console.log(numberList);
+});
+
+function appendOperation(amount) {
+  const row = document.createElement('span');
+  if (amount >= 0) {
+    row.innerText = ` +${amount}`;
+    row.style.color = 'green';
+  } else {
+    row.innerText = ` ${amount}`;
+    row.style.color = 'red';
+  }
+  operations.appendChild(row);
+  numberList.push(amount);
+  console.log(numberList);
+}
+
